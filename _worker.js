fe07d5f5a90d2bec1dc40df4ed89611a8ead0a72
LXN25565 <lixin25565@gmail.com>
2024-08-11 15:23:14 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'e564ea51-d060-4df7-8c1a-c4f7579d4e9a';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
